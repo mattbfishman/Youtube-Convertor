f5b9e59400236b9ea418181bf2491b92c1b8f6b6
mattbfishman <mbfishman@quinnipiac.edu>
2017-01-09 20:14:14 -0500

    Fix gui and add best/worst audio quality option

--- converter.py
+++ converter.py
@@ -1,30 +1,48 @@
 from Tkinter import *
 import subprocess
 
-def callback():
-	link =  e1.get()
+def callback(radioButtonValue, textBoxvalue):
+	if (radioButtonValue == 1):
+		qualityString = 'bestaudio/best'
+	else:
+		qualityString = 'worstaudio/worst'
+		 
 	try:
-		subprocess.check_output('youtube-dl -f "(bestaudio/best)[protocol^=http]" --extract-audio --audio-format mp3 --output "%(title)s.%(ext)s"' + " " + link , shell=True)
+		subprocess.check_output('youtube-dl -f "(' + qualityString + ')[protocol^=http]" --extract-audio --audio-format mp3 --output "%(title)s.%(ext)s" ' + textBoxvalue , shell=True)
 	except subprocess.CalledProcessError as e:
 		print ('Link is invalid')
+		e1.highlightcolor="red"
 
-root = Tk()
-root.wm_title("Youtube to Mp3")
+def makeGUI():
+	root = Tk()
+	root.wm_title("Youtube to Mp3")
 
-# v = IntVar()
-
-# Radiobutton(root, text="One", variable=v, value=1).pack(anchor=W)
-# Radiobutton(root, text="Two", variable=v, value=2).pack(anchor=W)
-
-e1 = Entry(root, bd = 1, highlightcolor="white")
-e1.insert(0, "Enter a link")
-e1.pack(anchor=W)
-
-MyButton1 = Button(root, text="Submit", width=10, command=callback)
-MyButton1.pack(anchor=W)
+	var = StringVar()
+	label = Label(root, textvariable=var)
+	var.set("Audio Quality")
+	label.pack(anchor=CENTER)
 
 
-# root.minsize(width=500, height=500)
-mainloop()
+	v = IntVar()
+	Radiobutton(root, text="Best", variable=v, value=1).pack(anchor=CENTER)
+	Radiobutton(root, text="Worst", variable=v, value=2).pack(anchor=CENTER)
+	v.set(1)
+
+	e1 = Entry(root, bd = 1, highlightcolor="white")
+	e1.insert(0, "Enter a link")
+	e1.pack(anchor=CENTER)
+
+	MyButton1 = Button(root, text="Submit", width=10, command= lambda: callback(v.get(), e1.get()))
+	MyButton1.pack(anchor=CENTER)
+
+
+	root.minsize(width=500, height=300)
+	mainloop()
+
+def Main():
+	makeGUI()
+
+if __name__ == '__main__':
+	Main()
 
 
